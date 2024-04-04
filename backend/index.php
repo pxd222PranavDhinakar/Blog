2ecb95f1e9bab4522d5afd75e053b5579cd269d8
Pranav Dhinakar <pranavdhinakar@gmail.com>
2024-04-04 18:15:00 +0000

    Added file management system

--- backend/index.php
+++ backend/index.php
@@ -1,5 +1,5 @@
 <?php
-header('Access-Control-Allow-Origin: http://localhost:3000');
+header('Access-Control-Allow-Origin: http://localhost:8000');
 header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type, Accept');
 
@@ -12,7 +12,10 @@ $curl = curl_init();
 curl_setopt_array($curl, [
     CURLOPT_URL => "https://api.github.com/repos/pxd222PranavDhinakar/Blog/languages",
     CURLOPT_RETURNTRANSFER => true,
-    CURLOPT_USERAGENT => 'PHP Application'
+    CURLOPT_USERAGENT => 'PHP Application',
+    CURLOPT_HTTPHEADER => [
+        'Authorization: Bearer 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'
+    ]
 ]);
 
 $response = curl_exec($curl);
@@ -22,10 +25,14 @@ curl_close($curl);
 $languages = json_decode($response, true);
 
 // Fetch commit history
+$curl = curl_init();
 curl_setopt_array($curl, [
     CURLOPT_URL => "https://api.github.com/repos/pxd222PranavDhinakar/Blog/commits",
     CURLOPT_RETURNTRANSFER => true,
-    CURLOPT_USERAGENT => 'PHP Application'
+    CURLOPT_USERAGENT => 'PHP Application',
+    CURLOPT_HTTPHEADER => [
+        'Authorization: Bearer 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'
+    ]
 ]);
 
 $response = curl_exec($curl);
@@ -35,6 +42,7 @@ curl_close($curl);
 $commits = json_decode($response, true);
 
 header('Content-Type: application/json');
+
 if ($err) {
     echo json_encode(["error" => "cURL Error #:" . $err]);
 } else {
